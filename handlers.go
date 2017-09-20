0787a1c312e3a6865e9da97d676b61de6c569b9e
henryhamon <henry@hamonrails.com.br>
2017-09-20 13:18:29 -0300

    first commit

---
+++ handlers.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome! I'm Puchalski a weather bot")
+}
